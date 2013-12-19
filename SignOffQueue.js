85b1f54e200ab064cf58ea59f0d25206646c3074
Rick Wieman <admin@rickw.nl>
2013-12-19 19:07:09 +0100

    Adds buttons to switch between TA and Student mode. Also adds basic assign functionality.

--- SignOffQueue.js
+++ SignOffQueue.js
@@ -1,18 +1,31 @@
 Students = new Meteor.Collection("students");
 
 if (Meteor.isClient) {
+  Template.openingPage.showContent = function() {
+    var template = (!Session.get('template') ? 'student' : Session.get('template'));
+
+    return Template[template]();
+  }
+  Template.openingPage.events({
+    'click #ta': function() {
+      Session.set('template', 'ta');
+    },
+    'click #student': function() {
+      Session.set('template', 'student');
+    }
+  });
+
   Template.ta.student = function() {
-    return Students.findOne({});
+    return Students.findOne({ assistant: {$exists: false}});
   }
   Template.ta.events({
     'click #accept' : function () {
-      console.log("Deleting " + Template.ta.student().cpmGroup);
-      Meteor.call("deleteGroup", Template.ta.student().cpmGroup);
+      Meteor.call("assignGroup", Template.ta.student().cpmGroup, 1);
     }
   });
 
   Template.student.studentQueue = function() {
-    return Students.find({});
+    return Students.find({ assistant: {$exists: false} });
   }
 
   Template.student.showForm = function() {
@@ -27,7 +40,7 @@ if (Meteor.isClient) {
 
       if(location && cpmGroup) {
         if(Students.find({ cpmGroup: cpmGroup }).fetch().length == 0) {
-          Students.insert({ location: location, cpmGroup: cpmGroup});
+          Meteor.call("insertGroup", cpmGroup, location);
         }
         Session.set("subscribed", true);
       }
@@ -51,8 +64,20 @@ if (Meteor.isServer) {
   });
 
   Meteor.methods({
-    deleteGroup: function(cpmGroup) {
-      Students.remove({cpmGroup: cpmGroup});
+    assignGroup: function(cpmGroup, ta) {
+      Students.update({cpmGroup: cpmGroup}, {$set: {assistant: ta}});
+    },
+
+    insertGroup: function(cpmGroup, location) {
+      Students.insert({cpmGroup: cpmGroup, location: location});
+    },
+
+    approveGroup: function(cpmGroup) {
+      Students.update({cpmGroup: cpmGroup}, {$set: {approved: true}});
+    },
+
+    rejectGroup: function(cpmGroup) {
+      Students.update({cpmGroup: cpmGroup}, {$set: {approved: false}});
     }
   });
 }
