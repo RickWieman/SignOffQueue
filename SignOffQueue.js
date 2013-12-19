f415dc88e26d13701025da2c7e1ee25e94ba98f6
Rick Wieman <admin@rickw.nl>
2013-12-19 17:32:20 +0100

    Yay some initial stuff.

--- SignOffQueue.js
+++ SignOffQueue.js
@@ -1,13 +1,46 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to SignOffQueue.";
-  };
+Students = new Meteor.Collection("students");
 
-  Template.hello.events({
-    'click input' : function () {
+if (Meteor.isClient) {
+  Template.ta.student = function() {
+    return Students.findOne({});
+  }
+  Template.ta.events({
+    'click #accept' : function () {
+      console.log("Deleting " + Template.ta.student().cpmGroup);
+      Meteor.call("deleteGroup", Template.ta.student().cpmGroup);
+    }
+  });
+
+  Template.student.studentQueue = function() {
+    return Students.find({});
+  }
+
+  Template.student.showForm = function() {
+    return !Session.get("subscribed");
+  }
+
+  Template.student.events({
+    'click #submit' : function () {
       // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+      var location = $('#loc').val();
+      var cpmGroup = $('#cpm').val();
+
+      if(location && cpmGroup) {
+        if(Students.find({ cpmGroup: cpmGroup }).fetch().length == 0) {
+          Students.insert({ location: location, cpmGroup: cpmGroup});
+        }
+        Session.set("subscribed", true);
+      }
+    },
+    'keyup #cpm' : function() {
+      var cpmGroup = $('#cpm').val();
+
+      if(Students.find({ cpmGroup: cpmGroup }).fetch().length != 0) {
+        $('#cpm').addClass('invalid');
+      }
+      else {
+        $('#cpm').removeClass('invalid');
+      }
     }
   });
 }
@@ -15,5 +48,11 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+  });
+
+  Meteor.methods({
+    deleteGroup: function(cpmGroup) {
+      Students.remove({cpmGroup: cpmGroup});
+    }
   });
 }
