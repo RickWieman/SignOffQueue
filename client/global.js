5889766ea243294a26b1b599b3aa4c50fdc2704e
Rick Wieman <admin@rickw.nl>
2014-01-09 19:41:45 +0100

    Adds nicer menu with first implementation of active page highlighting.

--- client/global.js
+++ client/global.js
@@ -3,11 +3,23 @@ Template.openingPage.showContent = function() {
 
   return Template[template]();
 }
+
+Template.openingPage.menuItems = function() {
+	return ['TA', 'Student'];
+}
+
+Template.openingPage.listMenuItems = function() {
+	if(this.toLowerCase() == Session.get('template')) {
+		return '<li id="'+ this.toLowerCase() +'" class="active"><a href="#">' + this + '</a></li>';
+	}
+	return '<li id="'+ this.toLowerCase() +'"><a href="#">' + this + '</a></li>';
+}
+
 Template.openingPage.events({
-  'click #ta': function() {
+  'click #ta a': function() {
     Session.set('template', 'ta');
   },
-  'click #student': function() {
+  'click #student a': function() {
     Session.set('template', 'student');
   }
 });
