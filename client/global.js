ec79407aa6dcec018eed7689fd7d079bac0f64e1
Rick Wieman <admin@rickw.nl>
2014-02-14 16:05:43 +0100

    Adds configuration and removes no longer used methods from global.js

--- client/global.js
+++ client/global.js
@@ -1,25 +1,4 @@
-Template.openingPage.showContent = function() {
-	if(!Session.get('template')) {
-		Session.set('template', 'student');
-	}
-  
-  	return Template[Session.get('template')]();
-}
-
-Template.openingPage.menuItems = ['TA', 'Student'];
-
-Template.openingPage.listMenuItems = function() {
-	if(this.toLowerCase() == Session.get('template')) {
-		return '<li id="'+ this.toLowerCase() +'" class="active"><a href="#">' + this + '</a></li>';
-	}
-	return '<li id="'+ this.toLowerCase() +'"><a href="#">' + this + '</a></li>';
-}
-
-Template.openingPage.events({
-  'click #ta a': function() {
-    Session.set('template', 'ta');
-  },
-  'click #student a': function() {
-    Session.set('template', 'student');
-  }
+Accounts.config({
+	forbidClientAccountCreation: true,
+	loginExpirationInDays: null
 });
